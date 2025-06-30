1b1fa17c951b38d770aba91cc2a8474183f6e759
Roy Quilor <royquilor@gmail.com>
2025-06-30 14:56:32 +0100

    Cleanup build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
-import Image from "next/image";
+import React from 'react';
 import CampaignTable from '../components/CampaignTable';
 
 export default function Home() {
